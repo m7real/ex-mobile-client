e3938482ec13226d052de878085ea02df5dcacd3
m7real <masayeedreal@gmail.com>
2022-11-25 21:55:33 +0600

    faq added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <img src={cover} className="lg:w-1/2 rounded-lg" alt="" />
         <div className="lg:w-1/2">
           <h1 className="text-5xl font-bold ">
-            SELL YOUR <br /> SMARTPHONE <br /> FOR QUICK CASH
+            SELL YOUR <br /> <span className="text-accent">SMARTPHONE</span> <br /> FOR QUICK CASH
           </h1>
           <p className="py-6">
             Are you looking for a place where you can buy or sell used mobile phone? Congratulations!! You are at the right place.
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import FAQ from "../FAQ/FAQ";
 import Stat from "../Stat/Stat";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Stat></Stat>
+      <FAQ></FAQ>
     </div>
   );
 };
