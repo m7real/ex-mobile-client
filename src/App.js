15b963c6efb8e64eea9f246750c714e0c52f61ff
m7real <masayeedreal@gmail.com>
2022-11-25 16:55:58 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./Routes/Routes/Routes";
 
 function App() {
   return (
-    <div>
+    <div className="lg:mx-11">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,21 @@
 import React from "react";
+import cover from "../../../assets/images/cover.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>This is banner</h2>
+    <div className="hero min-h-screen ">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={cover} className="lg:w-1/2 rounded-lg" alt="" />
+        <div className="lg:w-1/2">
+          <h1 className="text-5xl font-bold ">
+            SELL YOUR <br /> SMARTPHONE <br /> FOR QUICK CASH
+          </h1>
+          <p className="py-6">
+            Are you looking for a place where you can buy or sell used mobile phone? Congratulations!! You are at the right place.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
