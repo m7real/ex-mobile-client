422bd77075a6114446160c9e3bc5e50dc34e2978
m7real <masayeedreal@gmail.com>
2022-11-25 11:15:11 +0600

    firebase adde

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="">
       <h2>Hello</h2>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
