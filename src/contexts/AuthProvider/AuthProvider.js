92a84f4a9a2ec8875582bc64d0b02176a82716ab
m7real <masayeedreal@gmail.com>
2022-11-26 11:04:22 +0600

    AuthProvider added

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,83 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../../firebase/firebase.config";
+
+// context API
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  // creates user while signing up
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // update user profile info
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  // login
+  const logIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // login with google
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // log out
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    setLoading,
+    createUser,
+    updateUserProfile,
+    logIn,
+    signInWithGoogle,
+    logOut,
+  };
+
+  return (
+    // prettier-ignore
+    <AuthContext.Provider value={authInfo}>
+  {children}
+</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
