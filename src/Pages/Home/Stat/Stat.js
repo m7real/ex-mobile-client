5dc2513630f387e346d037f41bbd7730a4035bbb
m7real <masayeedreal@gmail.com>
2022-11-28 19:54:09 +0600

    stats updated

--- src/Pages/Home/Stat/Stat.js
+++ src/Pages/Home/Stat/Stat.js
@@ -1,28 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import Spinner from "../../../components/Spinner/Spinner";
 
 const Stat = () => {
+  const { data: stats = {}, isLoading } = useQuery({
+    queryKey: ["stats"],
+    queryFn: async () => {
+      const data = await axios.get("http://localhost:5000/stats");
+      return data.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Spinner small={true}></Spinner>;
+  }
+
   return (
-    <section className="flex justify-center my-10 py-6 shadow-xl border border-primary rounded-3xl">
-      <div className="stats stats-vertical lg:stats-horizontal ">
-        <div className="stat">
-          <div className="stat-title">Products</div>
-          <div className="stat-value text-primary">31K</div>
-          <div className="stat-desc">Jan 1st - Feb 1st</div>
-        </div>
+    <>
+      <h2 className="text-3xl font-bold">Statistics</h2>
+      <section className="flex justify-center my-10 py-6 shadow-xl border-b border-t border-accent rounded-3xl">
+        <div className="stats stats-vertical lg:stats-horizontal ">
+          <div className="stat">
+            <div className="stat-title">Total Products</div>
+            <div className="stat-value text-accent font-serif text-center">{stats.productsCount}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-title">New Buyers</div>
-          <div className="stat-value text-primary">4,200</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
-        </div>
+          <div className="stat">
+            <div className="stat-title">Total Users</div>
+            <div className="stat-value text-accent font-serif text-center">{stats.usersCount}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-title">New Sellers</div>
-          <div className="stat-value text-primary">1,200</div>
-          <div className="stat-desc">↘︎ 90 (14%)</div>
+          <div className="stat">
+            <div className="stat-title">Total Bookings</div>
+            <div className="stat-value text-accent font-serif text-center">{stats.bookingsCount}</div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
